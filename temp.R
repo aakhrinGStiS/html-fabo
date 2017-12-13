90dd02b254e5bb6a848cf280b9fab90aca21b2f5
Srik G <sskanthgorthy@gmail.com>
2017-12-13 13:21:34 +0530

    Update temp.R

--- temp.R
+++ temp.R
@@ -1,18 +1,1 @@
-html_curator <- function(data_frame, col_num, row_num){
-        temp <- data_frame[row_num, col_num]
-        
-        temp <- unlist(strsplit(temp, split = "<li class"))
-        
-        temp_num <- sapply(
-                1:length(temp), 
-                function(i) length(grep(
-                        temp[i], 
-                        pattern = "data-collection-item"))
-                )
-        
-        temp <- temp[which(temp_num == 1)]
-        
-        if(length(temp) != 0){
-        
-        }
-}
+
