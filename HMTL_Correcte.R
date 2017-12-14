38627e10a8773802f31e425357c5ac353ef0af27
Srik G <sskanthgorthy@gmail.com>
2017-12-14 19:51:44 +0530

    Update HMTL_Correcte.R

--- HMTL_Correcte.R
+++ HMTL_Correcte.R
@@ -1,1 +1,23 @@
-
+html_Correcter <- function(temp){  
+  temp_num <- grep(temp, pattern = "fref")
+  temp[temp_num] <- sapply(
+    temp_num,
+    function(i) unlist(strsplit(temp[i], split = "[?|&]fref"))[1]
+    )
+  
+  temp_char <- sapply(
+    1:length(temp),
+    function(i) substr(
+      temp[i],
+      start = nchar(temp[i]),
+      stop = nchar(temp[i]))
+    )
+  
+  temp_num <- which(temp_char != "/")
+  temp[temp_num] <- sapply(
+    temp_num,
+    function(i) paste0(temp[i], "/")
+    )
+  
+  return(temp)
+}
