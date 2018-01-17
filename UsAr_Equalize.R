6e512bd4bd706192e48e23e0b1a1a9394b7bfba5
Srik G <sskanthgorthy@gmail.com>
2018-01-17 16:34:32 +0530

    Update UsAr_Equalize.R

--- UsAr_Equalize.R
+++ UsAr_Equalize.R
@@ -1,3 +1,8 @@
+temp <- unique(collated_merged[,c(2,5)])
+temp <- temp[order(temp[,2]),]
+temp_num <- table(temp[,2])
+temp <- names(temp_num[temp_num > 1])
+
 UsAr_equalizer <- function(data_frame, username){
   
   data_temp <- unique(data_frame[
@@ -17,3 +22,14 @@ UsAr_equalizer <- function(data_frame, username){
   
   return(data_list_new)
 }
+
+data_list <- lapply(
+  1:length(temp),
+  function(i) UsAr_equalizer(collated_merged, temp[i])
+  )
+data_list_new <- do.call(
+  rbind,
+  data_list
+)
+
+collated_merged1 <- rbind(collated_merged, data_list_new)           
