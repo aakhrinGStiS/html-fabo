475b8de3cf713fe58aef051ee495e342139c5ad1
Srik G <sskanthgorthy@gmail.com>
2018-01-17 16:01:34 +0530

    Update UsAr_Equalize.R

--- UsAr_Equalize.R
+++ UsAr_Equalize.R
@@ -1,3 +1,19 @@
-function(username){
-
+UsAr_equalizer <- function(data_frame, username){
+  
+  data_temp <- unique(data_frame[
+    data_frame$cleaned.User.Id == username, 5:9
+  ])
+  
+  artists <- unique(
+    data_frame[
+      data_frame$cleaned.User.Id == username, 1:4]
+  )
+  
+  data_list <- lapply(1:length(artists), 
+                      function(i) cbind(artists[i,], data_temp)
+                      )
+  
+  data_list_new <- unique(do.call(rbind, data_list))
+  
+  return(data_list_new)
 }
